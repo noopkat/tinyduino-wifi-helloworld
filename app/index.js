bb0ec727f035cc771b32cf267c47c3a4e14a14b8
Suz Hinton <suz.hinton@gmail.com>
2014-10-03 16:07:56 -0700

    implement twilio

--- app/index.js
+++ app/index.js
@@ -1,4 +1,8 @@
 var Hapi = require('hapi');
+var twilioSid = '';
+var twilioToken = '';
+var twilioNums = { from: '', to: '' };
+var Twilio = require('twilio')(twilioSid, twilioToken);
 
 // fill this in with the token your tinyduino will send as part of the request
 var passToken = '558822';
@@ -16,12 +20,27 @@ server.route({
 
 server.route({
     method: 'GET',
-    path: '/mailbox/token/{token}',
+    path: '/mailbox/token/{token?}',
     handler: function (request, reply) {
       if (request.params.token && request.params.token === passToken) {
         reply('you\'ve got mail!');
 
         // dispatch to Twilio or MailGun here
+
+        // Twilio
+        Twilio.messages.create({
+            body: 'you have mail!',
+            to: twilioNums.to,
+            from: twilioNums.from
+        }, function(err, message) {
+            if (err) {
+              process.stdout.write(err);
+            }
+            process.stdout.write(message.sid);
+        });
+
+        // MailGun
+
 
       } else {
         reply('why you gotta troll me, friend :(');
